c74ab71a5a64c172e754da7582e91ede011bcf30
Jeremy Taylor <tay16036@byui.edu>
2018-01-27 21:40:46 -0700

    fixed browsing page

--- web/shopping/browse.php
+++ web/shopping/browse.php
@@ -1,4 +1,5 @@
 <?php
+//start the session
 session_start();
 ?>
 
@@ -20,26 +21,6 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 </head>
 <body>
-	<?php
-	// define variables and set to empty values
-	$vegetableErr = "";
-	$vegetable = "";
-
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (empty($_POST["vegetable"])) {
-			$vegetableERR = "You must choose a vegetable";
-		} else {
-			$vegetable = test_input($_POST["vegetable"]);
-		}
-	}
-
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-	?>
 	<header id="page_header">
 		<figure><img src="img/logo.png" alt="logo"></figure> <!-- Logo made with Flower graphic by <a href="undefined">undefined</a> from <a href="https://logomakr.com/">Logomakr</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>. Check out the new logo that I created on <a href="http://logomakr.com" title="Logo Makr">LogoMakr.com</a> https://logomakr.com/6nuXey-->
 		<h1>Shopping</h1>
@@ -48,26 +29,27 @@ session_start();
         <ul id="mainmenu">
     	<li class="active"><a href="browse.php">Browse Products</a></li>
     	<li><a href="cart.php">View Cart</a></li>
-</ul>
+		</ul>
     </nav>
 	<main>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<form method="post" action="">
 			<p>Browse your favorite vegetables here:</p>
 
 			Vegetables:<br />
 			<input type="radio" name="vegetable" value="Carrot" id="vegetable-carrot"><label for="vegetable-carrot">Carrots</label><br />
 			<input type="radio" name="vegetable" value="Celery" id="vegetable-celery"><label for="vegetable-celery">Celery</label><br />
 			<input type="radio" name="vegetable" value="Cucumber" id="vegetable-cucumber"><label for="vegetable-cucumber">Cucumbers</label><br />
-			<input type="radio" name="vegetable" value="Squash" id="vegetable-squash"><label for="vegetable-squash">Squash</label><span class="error">* <?php echo $vegetableErr;?></span><br />
+			<input type="radio" name="vegetable" value="Squash" id="vegetable-squash"><label for="vegetable-squash">Squash</label><span class="error"><br />
 			<br />
 
-			<input type="submit" value="Add to Cart">
+			<input type="submit" name="Submit" value="Add to Cart">
 		</form>
-
 		<?php
-		$_SESSION["choice"] = vegetable.value;
-		echo "You have added " . $choice . "to your cart."
+		if (isset($_POST)['Submit']) {
+			$_SESSION['vegetable'] = $_POST['vegetable'];
+		}
 		?>
+		<strong>You added<?php echo $_SESSION['vegetable'];?> to your cart.</strong>
 	</main>
 	<footer>
         <p>&copy; <?php echo date("Y"); ?> Jeremy Taylor. All rights reserved.</p>
