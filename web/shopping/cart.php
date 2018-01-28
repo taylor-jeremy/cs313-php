eb40f947e8c7e11ed4c2d928d5b77143830a7dbc
Jeremy Taylor <tay16036@byui.edu>
2018-01-27 22:21:35 -0700

    modified browsing page

--- web/shopping/cart.php
+++ web/shopping/cart.php
@@ -48,8 +48,9 @@ session_start();
 		$user_arr = array();
 		if (isset($_POST['Submit'])) {
 			$_SESSION['vegetable'] = $_POST['vegetable'];
-			$user_arr = $_SESSION['vegetable'];
 		}
+		$user_arr = $_SESSION['vegetable'];
+		$_SESSION[$user_arr] = $user_arr;
 		?>
 		<strong>You added <?php echo $_SESSION[$user_arr]; ?> to your cart. View your <a href="cart.php">cart</a>.</strong>
 	</main>
