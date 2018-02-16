728fb04f6d37f93df3316a78f901303819782620
Jeremy Taylor <tay16036@byui.edu>
2018-02-16 13:26:43 -0700

    modified view_cars.php and added database.php

--- web/view_cars.php
+++ web/view_cars.php
@@ -6,26 +6,8 @@
 * Description: This file queries a
 *   PostgreSQL database from PHP.
 ***********************************************************/
-// It would be better to store these in a different file
-$dbUser = 'postgres';
-$dbPassword = 'student';
-$dbName = 'carsdb';
-$dbHost = 'localhost';
-$dbPort = '5433';
-try
-{
-	// Create the PDO connection
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex)
-{
-	// If this were in production, you would not want to echo
-	// the details of the exception.
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
+include "database.php";
+$db = get_db();
 ?>
 <!DOCTYPE html>
 <html>
